06b950f06ff4b43b5be727d6a04575ed65ca2129
dahwiya <130633407+dahwiya@users.noreply.github.com>
2024-09-25 17:57:03 +0900

    Add files via upload

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "JungDaHwe-Linju-1.0";
+const contentToCache = [
+    "Build/BuildWebGL.loader.js",
+    "Build/BuildWebGL.framework.js.br",
+    "Build/BuildWebGL.data.br",
+    "Build/BuildWebGL.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
